064e72ad6cba885cebfc714cc3939ff484e39c30
amaxamall <koychevaalexandra@gmail.com>
2024-01-05 13:05:41 +0200

    Deleting logout.php

--- components/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include 'connection.php';
-
-	//Log out-deleting the cookie and redirecting to all posts
-	setcookie('user_id', '', time() - 1, '/');
-	header('location:../all_posts.php');
-?>
